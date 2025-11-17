bb4b1451cf89bbd2120741c6bf3258fac918311d
Flow-134 <carlos.barroso20@aluno.ce.gov.br>
2025-11-17 11:09:16 -0300

    atividade nova 17/11

---
+++ funçoes.py
@@ -0,0 +1,10 @@
+def carregar_cardapio(cardapio):
+    cardapio =[
+        {"ID" : 1, "Hamburguer" : "Hamburguer" , "preço" : 12.50},
+        {"ID" : 1, "Pizza" : "Pizza" , "preço" : 35},
+        {"ID" : 1, "Refrigerante" : "Refrigerante" , "preço" : 5}
+]
+
+def exibir_cardapio(cardapio):
+    return cardapio
+
